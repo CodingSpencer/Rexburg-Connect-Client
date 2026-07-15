ede2f299226c26fb7a7a0ce4fe42ffde70baba63
Spencer <sashcraft121@gmail.com>
2026-07-15 08:20:49 -0600

    astro config added to frontend

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rexburg-connect-client.netlify.app",
   integrations: [svelte()]
 });
